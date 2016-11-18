cffb41dfca94634db74d7ed2c61c23a1d41ed1db
d0xkb <d0xkb@protonmail.ch>
2016-11-18 20:29:32 +0100

    few small changes

--- run.sh
+++ run.sh
@@ -19,13 +19,13 @@ services="hciuart.service bluetooth.service avahi-daemon.service fake-hwclock.se
 apt-get update
 apt-get -y upgrade
 
-#install additional packages
+# install additional packages
 apt-get -y install $packages
 
-#disable few services
+# disable unneeded services
 systemctl disable $services
 
-#create and mount ramdisk
+# create and mount ramdisk
 mkdir /ramdisk
 cat >> /etc/fstab <<EOF
 
@@ -63,10 +63,10 @@ if [[ $(systemctl get-default) != "multi-user.target" ]]; then
   systemctl set-default multi-user.target
 fi
 
-#cleaning via apt
+# cleaning via apt
 apt-get autoclean
 apt-get -y autoremove
-echo "[i]apt clean, unused packages removed"
 
-#done, restart
+# done, restart
+tput bold; echo "done, rebooting"; tput sgr0
 shutdown -r now
